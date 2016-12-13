ace91dadd4b10dee57f2f8f94bcf7cfa5e50ea63
Claire Ryan <cryan@pcis.com>
2016-12-13 11:32:29 -0800

    Changed the Index action/response to use Twig

--- app/Domain/Index/IndexAction.php
+++ app/Domain/Index/IndexAction.php
@@ -17,40 +17,8 @@ class IndexAction extends WebAction
 
     public function index()
     {
-        $this->responder->insertOutputData('singlevariable', 'This is the loaded single variable');
-
-        $repeatable = array(
-            ['content' => 'This is the first content piece', 'alsoremoveme' => ' and this is a conditional content piece'],
-            ['content' => 'This is the second content piece']
-        );
-
-        $complex = array(
-            [
-                'repeatcanhavevarstoo' => 'Complex Header 1',
-                'firstrepeat' => [
-                    ['repeatthis' => 'http://www.google.com'],
-                    ['repeatthis' => 'http://www.facebook.com']
-                ],
-                'secondrepeat' => [
-                    ['repeatthis' => 'Repeating three'],
-                    ['repeatthis' => 'and repeating four']
-                ]
-            ],
-            [
-                'repeatcanhavevarstoo' => 'Complex Header 2',
-                'firstrepeat' => [
-                    ['repeatthis' => 'http://www.google.com'],
-                    ['repeatthis' => 'http://www.facebook.com']
-                ],
-                'secondrepeat' => [
-                    ['repeatthis' => 'Repeating seven'],
-                    ['repeatthis' => 'and repeating eight']
-                ]
-            ]
-        );
-
-        $this->responder->insertOutputData('repeatable', $repeatable);
-        $this->responder->insertOutputData('complex', $complex);
+        $welcomestring = date('l jS \of F Y', time());
+        $this->responder->insertOutputData('welcome', $welcomestring);
         $this->responder->index();
     }
 }
--- app/Domain/Index/IndexResponder.php
+++ app/Domain/Index/IndexResponder.php
@@ -7,7 +7,7 @@ use Chassis\Response\WebResponse;
 class IndexResponder extends WebResponse
 {
     public function index() {
-        $this->setTemplate(file_get_contents('index.html'));
+        $this->setTemplate('index.html');
         $this->out();
     }
 }
