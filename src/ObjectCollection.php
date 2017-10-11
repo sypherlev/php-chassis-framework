36c8b13e1bbd4c3708b61fc3ee753de97e72c1d7
Claire Ryan <cryan@pcis.com>
2017-10-11 14:29:29 -0700

    Cleaned up the object management

--- src/ObjectCollection.php
+++ src/ObjectCollection.php
@@ -6,7 +6,6 @@ use SypherLev\Chassis\Data\SourceBootstrapper;
 use App\Common\Security;
 use App\DBAL;
 use League\Container\Container;
-use SypherLev\Blueprint\QueryBuilders\MySql\MySqlQuery;
 
 class ObjectCollection extends Container
 {
@@ -14,13 +13,19 @@ class ObjectCollection extends Container
 
         parent::__construct();
 
-        // bootstrapper and sources
-        $this->add('bootstrapper', new SourceBootstrapper);
-        $this->add('local-source', $this->get('bootstrapper')->generateSource('local'));
+        // sources
+        $this->add('local-source', SourceBootstrapper::generateSource('local'));
 
         // DBAL entities
-        $this->add('auth-local', new DBAL\AuthData($this->get('local-source'), new MySqlQuery()));
-        $this->add('user-local', new DBAL\UserData($this->get('local-source'), new MySqlQuery()));
+        $this->add('auth-local', new DBAL\AuthData(
+            $this->get('local-source'),
+            $this->get('local-source')->generateNewQuery()
+        ));
+
+        $this->add('user-local', new DBAL\UserData(
+            $this->get('local-source'),
+            $this->get('local-source')->generateNewQuery()
+        ));
 
         // extended services
         $this->add('security', new Security($this->get('auth-local')));
