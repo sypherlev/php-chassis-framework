c139bbc71a11c733632a093e9c8f735d2f26a2d6
Claire Ryan <cryan@pcis.com>
2019-06-03 20:21:33 -0700

    Fixing another bloody cookie issue

--- src/Domain/Auth/AuthResponder.php
+++ src/Domain/Auth/AuthResponder.php
@@ -14,6 +14,8 @@ class AuthResponder extends ApiResponse
 
     public function signin($user) {
         if($user) {
+            setcookie('ch_ct', $user->cookietoken, time() + 60*60*24, "/", "", false, true);
+            unset($user->cookietoken);
             $this->setHTTPCode(200);
             $this->insertOutputData('user', $user);
             $this->setOutputMessage('User authenticated');
